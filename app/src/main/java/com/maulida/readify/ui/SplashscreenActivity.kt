16fbaeb79c2f14dbdab7ef8661af08ede03bc9b1
Maulida Nur Afifah <maulidaaa.nurafifah@gmail.com>
2024-01-11 09:33:30 +0700

    update

---
+++ app/src/main/java/com/maulida/readify/ui/SplashscreenActivity.kt
@@ -0,0 +1,10 @@
+package com.maulida.readify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
