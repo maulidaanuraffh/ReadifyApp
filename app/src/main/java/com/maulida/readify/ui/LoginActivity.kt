2d397ec9b602d9e375b031e55a51e2f3312aa519
Maulida Nur Afifah <maulidaaa.nurafifah@gmail.com>
2024-01-11 15:27:35 +0700

    update

---
+++ app/src/main/java/com/maulida/readify/ui/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.maulida.readify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/maulida/readify/ui/WecomePageActivity.kt
@@ -0,0 +1,10 @@
+package com.maulida.readify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WecomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
